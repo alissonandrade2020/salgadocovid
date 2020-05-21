8dbf8a4f171164336a4674293b8686f7ca48f6b3
Alisson de Andrade Araújo <39311340+alissonandrade2020@users.noreply.github.com>
2020-05-21 07:33:05 -0300

    Update numeros.js

--- src/pages/numeros.js
+++ src/pages/numeros.js
@@ -37,14 +37,14 @@ export default function Numeros() {
     )
       .then(res => res.json())
       .then(res => {
-        const { cases, confirmed, deaths, recovered, date } = res.results[0]
+        const { confirmed, deaths, date } = res.results[0]
 
         setPb({
           title: "Paraíba",
           confirmed,
-          cases,
+          cases: "Sem informação",
           deaths,
-          recovered,
+          recovered: "Sem informação",
           updated_at: date,
         })
       })
